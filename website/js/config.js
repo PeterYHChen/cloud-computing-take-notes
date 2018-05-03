0b0ad6a3401f35f2a35a0ca3228570fc2e52f079
yonghong <yonghong.chenn@gmail.com>
2018-05-03 15:54:38 -0400

    Update user pool

--- website/js/config.js
+++ website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ZaApFZSMH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6d0f563hoo3p8j7514u7d38ded', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
